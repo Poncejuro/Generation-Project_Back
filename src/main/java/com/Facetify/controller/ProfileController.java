77dd7e6a0caf832bad1aeb98c0d318c54e5647fe
Andres <eacq.andres@gmail.com>
2023-09-27 18:34:34 -0600

    get profiles events

--- src/main/java/com/Facetify/controller/ProfileController.java
+++ src/main/java/com/Facetify/controller/ProfileController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/profiles")
+@CrossOrigin(origins = "*")
 @Log4j2 // to print in console
 public class ProfileController {
 
--- src/main/java/com/Facetify/controller/PublicationController.java
+++ src/main/java/com/Facetify/controller/PublicationController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/publications")
+@CrossOrigin(origins = "*")
 @Log4j2 // to print in console
 public class PublicationController {
 	@Autowired
